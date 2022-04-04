6b22a930520bac5174e3278d41445cc5dcde50d7
marialuisa <mdazatorres@cimat.mx>
2022-04-04 14:49:53 -0700

    Initial commit

---
+++ dashboard_WW.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 14:51:08 2022
+
+@author: marialuisa
+"""
+import pandas as pd
+import numpy as np
+import streamlit as st
+from plot_conc import plotN1N2, plotPMMoV, plotN
+from aux_function import is_authenticated, generate_login_block, clean_blocks, login
+import os
+
+theme = "dark"
+border_color = "lightgrey"
+repo = f"theming-showcase-{theme}"
+GITHUB_OWNER = "streamlit"
+
+
+
+st.set_page_config(layout="wide", initial_sidebar_state="auto", page_title=None, page_icon=None)
+
+data_ww_Davis = pd.read_csv('data_ww_Davis_loc.csv', index_col=0)
+data_ww= pd.read_csv('data_ww_cases_full.csv')
+localities = list(data_ww_Davis.Location.unique())[:-1]
+
+def data_loc(loc):
+    city_data = data_ww_Davis[data_ww_Davis['Location'] == loc]
+    city_data = city_data.groupby('SampleDate').sum()
+    city_data = city_data.reset_index()
+    return city_data
+
+def data_city(city, data_ww):
+    city_data = data_ww[data_ww['City'] == city]
+    city_data = city_data.reset_index()
+    return city_data
+
+city_data = data_city('Davis', data_ww)
+city_data['SampleDate'] = pd.to_datetime(city_data['SampleDate'])
+
+
+def main():
+    with open('styles.css') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+    st.header('Neighborhood Wastewater Data')
+    st.sidebar.header('SELECT A LOCATION')  # --------
+    loc = st.sidebar.selectbox('', localities)
+    loc_data = data_loc(loc)
+
+    loc_data['SampleDate'] = pd.to_datetime(loc_data['SampleDate'])
+    start_date = loc_data['SampleDate'].min().to_pydatetime()
+    end_date = loc_data['SampleDate'].max().to_pydatetime()
+
+    st.sidebar.header('PLOT DETAILS')  # --------
+    col1, col2 = st.sidebar.columns([1, 0.51])
+    smooth = col1.selectbox('Smoothing function', ['Trimmed average', 'Moving average', 'None'], index=1)
+
+    if smooth != 'None':
+        size_window = col2.selectbox('Size window', [5, 7], index=1)
+        center = col1.checkbox('Center')
+    else:
+        size_window = 0
+        center = False
+
+    log = st.sidebar.checkbox('Log transformation')
+    col1, _ = st.sidebar.columns([1, 0.1])
+    lag = col1.slider('Lag', min_value=0, max_value=14)
+
+   #  -------------------- Main -----------------
+    col1, _= st.columns([4, 0.1])
+
+    sl_init, sl_end = col1.slider('', min_value=start_date, max_value=end_date, value=(start_date, end_date),format='MMM DD, YYYY')
+
+    loc_data_ = loc_data[(loc_data['SampleDate'] >= sl_init) & (loc_data['SampleDate'] <= sl_end)]
+    city_data_= city_data[(city_data['SampleDate'] >= sl_init) & (city_data['SampleDate'] <= sl_end)]
+
+    fig1 = plotN(city_data, loc_data_, log, smooth, size_window, center, lag=lag)
+    fig2 = plotPMMoV(loc_data_, log, smooth, size_window, center)
+    fig3 = plotN1N2(loc_data_, log, smooth, size_window, center)
+
+    col1.plotly_chart(fig1, use_container_width=True)
+
+    col1, col2 = st.columns([2, 2])
+    col1.plotly_chart(fig2, use_container_width=True)
+    col2.plotly_chart(fig3, use_container_width=True)
+
+    st.markdown(""" <style>[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {width: 280px;}
+        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 280px;margin-left: -300px;}
+        </style>""", unsafe_allow_html=True, )
+
+
+login_blocks = generate_login_block()
+password = login(login_blocks)
+
+if is_authenticated(password):
+    clean_blocks(login_blocks)
+    main()
+elif password:
+    st.info("Please enter a valid password")
+
+
+
+
+
+
+
---
+++ plot_conc.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 17:37:42 2022
+
+@author: marialuisa
+"""
+
+# import pandas as pd
+import numpy as np
+# import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from scipy.stats import trim_mean, pearsonr
+from sklearn.linear_model import LinearRegression
+
+
+fontsize = 14
+
+
+def plotN(city_data, loc_data, log, opt, size_window, center, lag):  # normalizado
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    nn = loc_data.SampleDate.shape[0]
+    x = loc_data.SampleDate[:nn - lag]
+    name = 'N/PMMoV (influent)'  # + ' (' + str(city_data.Type.unique()[0]) + ')'
+    name_ = 'City-wide N/PMMoV (influent)'
+    cases = False
+    if opt == 'Moving average':
+        fig.add_trace(go.Scatter(name=name, mode='lines', x=x,
+                                 y=loc_data['NormalizedConc'].rolling(window=size_window, center=center).mean()[ :nn - lag],
+                                 line=dict(color='black', width=3)), secondary_y=False, )
+
+        fig.add_trace(go.Scatter(name=name_, mode='lines', x=x,
+                                 y=city_data['NormalizedConc'].rolling(window=size_window, center=center).mean()[:nn - lag], line=dict(color='gray', width=3)), secondary_y=False, )
+        if cases:
+            fig.add_trace(go.Scatter(name='Cases', mode='lines', x=x, y=loc_data['positives'].rolling(window=size_window, center=center).mean()[lag:],
+                                     line=dict(color='gray', width=3)), secondary_y=True, )
+
+    elif opt == 'Trimmed average':  # .apply(lambda x: trim_mean(x, 0.2))
+        fig.add_trace(go.Scatter(name=name, mode='lines', x=x,
+                                 y=loc_data['NormalizedConc'][:nn - lag].rolling(window=size_window, center=center).apply(lambda x: trim_mean(x, 1 / size_window)),
+                                 line=dict(color='black', width=3)), secondary_y=False, )
+        fig.add_trace(go.Scatter(name=name_, mode='lines', x=x, y=city_data['NormalizedConc'][:nn - lag].rolling(window=size_window, center=center).apply(
+                                     lambda x: trim_mean(x, 1 / size_window)), line=dict(color='black', width=3)), secondary_y=False, )
+        if cases:
+            fig.add_trace(go.Scatter(name='Cases', mode='lines', x=x, y=loc_data['positives'][lag:].rolling(window=size_window, center=center).apply(
+                                         lambda x: trim_mean(x, 1 / size_window)), line=dict(color='gray', width=3)), secondary_y=True, )
+    else:
+        # if not cases_county:
+        #    fig.add_trace(go.Scatter(name='Imputed', mode='markers', x=x, y=city_data['NormalizedConc'][:nn - lag],
+        #                         marker=dict(color='orange')), secondary_y=False, )
+        fig.add_trace(go.Scatter(name=name, mode='lines+markers', x=x, y=loc_data['NormalizedConc'][:nn - lag],
+                                 marker=dict(color='black', size=8), line=dict(color='black', width=3)),
+                      secondary_y=False, )
+        fig.add_trace(go.Scatter(name=name_, mode='lines+markers', x=x, y=city_data['NormalizedConc'][:nn - lag],
+                                 marker=dict(color='gray', size=8), line=dict(color='gray', width=3)),
+                      secondary_y=False, )
+        # city_data['NormalizedConc_crude'][:nn - lag]
+        # fig.update_layout(showlegend=False)
+        if cases:
+            fig.add_trace(go.Scatter(name='Cases', mode='lines', x=x, y=loc_data['positives'][lag:],
+                                     line=dict(color='gray', width=3)), secondary_y=True, )
+
+    if log:
+        fig.update_yaxes(type="log")
+        fig.update_layout(yaxis=dict(title="Log-Scale"))
+    if cases:
+        fig.update_yaxes(title_text="Cases", secondary_y=True)
+    fig.update_layout(font=dict(family="sans-serif", size=fontsize, color="black"), template="plotly_white",
+                      legend_font_size=14, legend_title_font_size=fontsize)
+    fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=0.5))
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+
+    fig.update_layout(autosize=False, width=450, height=250, margin=dict(l=0, r=0, b=10, t=10, pad=4),
+                      yaxis=dict(title='N/PMMoV'))  # ,xaxis=dict(title="Date"))
+    fig.update_layout(font_family="Arial", title_font_family="Arial")
+    fig.layout.showlegend = True
+    # fig.update_layout(font_color='white', title_font_color='white')
+
+    return fig
+
+
+
+def plotN1N2(city_data, log, opt, size_window, center):
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    if opt == 'Moving average':
+        fig.add_trace(go.Scatter(name='N1 gene', mode='lines', x=city_data.SampleDate,
+                                 y=city_data['N1_Concentration_Merged'].rolling(window=size_window, center=center).mean(),
+                                 line=dict(color='blue', width=3)), secondary_y=False,)
+        fig.add_trace(go.Scatter(name='N2 gene', mode='lines', x=city_data.SampleDate,
+                                 y=city_data['N2_Concentration_Merged'].rolling(window=size_window, center=center).mean(),
+                                 line=dict(color='red', width=3)), secondary_y=False, )
+    elif opt == 'Trimmed average':  # .apply(lambda x: trim_mean(x, 0.2))
+        fig.add_trace(go.Scatter(name='N1 gene', mode='lines', x=city_data.SampleDate,
+                                 y=city_data['N1_Concentration_Merged'].rolling(window=size_window, center=center).apply(
+                                     lambda x: trim_mean(x, 1 / size_window)), line=dict(color='blue', width=3)), secondary_y=False, )
+        fig.add_trace(go.Scatter(name='N2 gene', mode='lines', x=city_data.SampleDate,
+                                 y=city_data['N2_Concentration_Merged'].rolling(window=size_window,center=center).apply(
+                                     lambda x: trim_mean(x, 1 / size_window)), line=dict(color='red', width=3)), secondary_y=False, )
+    else:
+        fig.add_trace(go.Scatter(name='N1 gene', mode='lines+markers', x=city_data.SampleDate, y=city_data['N1_Concentration_Merged'],
+                       line=dict(color='blue', width=3),marker=dict(color='blue', size=5)), secondary_y=False, )
+
+        fig.add_trace(go.Scatter(name='N2 gene', mode='lines+markers', x=city_data.SampleDate, y=city_data['N2_Concentration_Merged'],
+                       line=dict(color='red', width=3),marker=dict(color='red', size=5)), secondary_y=False, )
+    if log:
+        fig.update_yaxes(type="log")
+        fig.update_layout(yaxis=dict(title="Logarithmic Scale"))
+    fig.update_layout(font=dict(family="sans-serif", size=fontsize, color="black"), template="plotly_white",
+                      legend_font_size=14, legend_title_font_size=fontsize)
+    fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=0.9))
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    fig.update_layout(autosize=False, width=450, height=250, margin=dict(l=0, r=0, b=10, t=10, pad=4),
+                      yaxis=dict(title='gc/ml wastewater'))  # ,xaxis=dict(title="Date"))
+    fig.update_layout(font_family="Arial", title_font_family="Arial")
+    #fig.update_layout(font_color='white', title_font_color='white')
+    return fig
+
+
+
+
+
+def plotPMMoV(city_data, log, opt, size_window, center):
+    # city_data=city_data[(city_data['SampleDate']>=start_date)&(city_data['SampleDate']<=end_date)]
+    fig = go.Figure()
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    name = 'PMMoV gc/g dry weight (influent)'
+
+    if opt == 'Moving average':
+        fig.add_trace(go.Scatter(name=name, mode='lines', x=city_data.SampleDate,
+                                 y=city_data['PV_Concentration_Merged'].rolling(window=size_window, center=center).mean(),
+                                 line=dict(color='green', width=3)), secondary_y=False, )
+    elif opt == 'Trimmed average':
+        fig.add_trace(go.Scatter(name=name, mode='lines', x=city_data.SampleDate,
+                                 y=city_data['PV_Concentration_Merged'].rolling(window=size_window, center=center).apply(lambda x: trim_mean(x, 1 / size_window)),
+                                 line=dict(color='green', width=3)), secondary_y=False, )
+    else:
+
+        fig.add_trace(go.Scatter(name=name, mode='lines+markers', x=city_data.SampleDate, y=city_data['PV_Concentration_Merged'], line=dict(color='green', width=3),marker = dict(color='green', size=5)), secondary_y=False, )
+    if log:
+        fig.update_yaxes(type="log")
+        fig.update_layout(yaxis=dict(title="Log-Scale"))
+    fig.layout.showlegend = True
+    fig.update_layout(font=dict(family="sans-serif", size=fontsize, color="black"), template="plotly_white",
+                      legend_font_size=14, legend_title_font_size=fontsize)
+    # fig.update_layout(autosize=False,width=500,height=250, margin=dict(l=0,r=0,b=10,t=10,pad=4),xaxis=dict(title="Date"))
+    fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=0.9))
+    fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
+    fig.update_layout(autosize=False, width=450, height=250, margin=dict(l=0, r=0, b=10, t=10, pad=4), yaxis=dict(title="gc/ml wastewater")) #"gc/ml wastewater"
+    fig.update_layout(font_family="Arial", title_font_family="Arial")
+    #fig.update_layout(font_color='white', title_font_color='white')
+    return fig
+
+
+
